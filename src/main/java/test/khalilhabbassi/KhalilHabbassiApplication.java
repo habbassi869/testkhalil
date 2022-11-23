a96fa55cc50c7eccf7b483d3f6af0015cf6598e1
habbassi869 <61434235+habbassi869@users.noreply.github.com>
2022-11-23 09:22:14 +0100

    first commit

---
+++ src/main/java/test/khalilhabbassi/KhalilHabbassiApplication.java
@@ -0,0 +1,13 @@
+package test.khalilhabbassi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhalilHabbassiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhalilHabbassiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/khalilhabbassi/KhalilHabbassiApplicationTests.java
@@ -0,0 +1,13 @@
+package test.khalilhabbassi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhalilHabbassiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
